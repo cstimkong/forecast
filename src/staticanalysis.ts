7dc91cd2c170c345d4b12f1fe200280a9506dc8b
Dezhen Kong <timkong@zju.edu.cn>
2025-09-07 21:01:46 +0800

    Update

---
+++ src/staticanalysis.ts
@@ -0,0 +1,111 @@
+/**
+ * 
+ * This file is part of FesaJS
+ */
+
+import fs from 'fs';
+
+import babelParser from '@babel/parser';
+import babelTraverse from '@babel/traverse';
+import babelGenerator from '@babel/generator';
+import objectHash from 'object-hash';
+import { ProgramLocation } from './instrumentation';
+
+export type Hint = {
+    start: ProgramLocation,
+    end: ProgramLocation,
+    filename: string,
+    content: {
+        type: string,
+        value: any
+    }
+}
+
+function getRuntimeHints(runtimeHints: Hint[], sourceCodeStart: number, sourceCodeEnd: number) {
+    let result = [];
+    for (let item of runtimeHints) {
+        if (item.start.index === sourceCodeStart && item.end.index == sourceCodeEnd) {
+            result.push({value: item.content.value, type: item.content.type});
+        }
+    }
+    return result;
+}
+
+/**
+ * 
+ * Attach the runtime hints to the AST
+ * @param {Array} fileSet 
+ * @param {Array} runtimeHints 
+ * @returns 
+ */
+function attachRuntimeHints(fileSet: Array<string>, runtimeHints: Array<any>) {
+    let astMap: any = {}
+    for (let f of fileSet) {
+        let content = fs.readFileSync(f, {encoding: 'utf-8'});
+        let ast = babelParser.parse(content, { sourceFilename: f });
+        babelTraverse(ast, {
+            exit: function(path) {
+                if (path.isExpression()) {
+                    let hints = getRuntimeHints(runtimeHints, path.node.start!, path.node.end!);
+                    (path.node as any).__hints__ = hints;
+                    (path.node as any).__evaluateto__ = {};
+                    (path.node as any).__funcdef__ = {};
+                    for (let h of (path.node as any).__hints__) {
+                        if (h.type === 'taintInfo') {
+                            (path.node as any).__tainted__ = true;
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+        });
+        astMap[f] = ast;
+    }
+    return astMap;
+}
+
+function propagate(astMap: NodeJS.Dict<any>): Array<any>{
+    // let updates = [];
+    for (let filename in astMap) {
+        let ast = astMap[filename];
+        babelTraverse(ast, {
+            enter(path) {
+                if (path.isMemberExpression() && ! (path.parentPath.isAssignmentExpression() && path.parentKey === 'left')) {
+                    if ((path.get('property').node as any).__tainted__) {
+                        let v = {internalObj: 'Object.prototype'};
+                        (path.node as any).__evaluateto__[objectHash(v)] = v;
+                    }
+                }
+
+                if (path.isAssignmentExpression()) {
+                    if (path.get('left').isIdentifier()) {
+                        for (let [k, v] of Object.entries((path.get('right').node as any).__evaluateto__)) {
+                            (path.get('left').node as any).__evaluateto__[k] = v;
+                        }
+                    }
+                    if (path.get('left').isExpression() && (path.get('left').node as any).__evaluateto__.hasOwnProperty(objectHash({internalObj: 'Object.prototyoe'}))) {
+                        if ((path.get('right').node as any).__tainted__) {
+                            /* Report prototype pollution */
+                            console.log({taintPosition: path.node.loc, content: babelGenerator(path.node).code});
+                        }
+                    }
+                }
+            }
+        })
+    }
+    // TODO
+    return [];
+    // return updates;
+}
+
+export function solve(sourceFiles: Array<string>, runtimeHints: Array<Hint>) {
+    let astMap = attachRuntimeHints(sourceFiles, runtimeHints);
+    while (true) {
+        let updates = propagate(astMap);
+        if (!updates || updates.length === 0) {
+            break;
+        }
+    }
+    return astMap;
+}
